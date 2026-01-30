f7936c1519ca9212311e26886ee9c0469ff99884
nairtanmay <nairtanmaybiju@email.com>
2026-01-30 15:56:24 +0530

    0

---
+++ app/detect/page.tsx
@@ -0,0 +1,14 @@
+'use client'; 
+export const dynamic = 'force-dynamic';
+
+import React from 'react';
+
+// You MUST have this "export default function" part
+export default function DetectPage() {
+  return (
+    <div>
+      <h1>Detect Page</h1>
+      {/* Your component logic here */}
+    </div>
+  );
+}
